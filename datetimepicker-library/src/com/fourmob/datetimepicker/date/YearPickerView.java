23d9c9e86b2b80d13e0a05254e6d93a4ef4c7567
lachlanm <lachlandevelopment@gmail.com>
2015-12-13 20:06:06 +1100

    Update the date picker year selector to reflect the new calendar date picker design.

--- datetimepicker-library/src/com/fourmob/datetimepicker/date/YearPickerView.java
+++ datetimepicker-library/src/com/fourmob/datetimepicker/date/YearPickerView.java
@@ -1,28 +1,32 @@
 package com.fourmob.datetimepicker.date;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
 import android.content.res.Resources;
-import android.graphics.drawable.StateListDrawable;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.fourmob.datetimepicker.R;
-import com.fourmob.datetimepicker.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class YearPickerView extends ListView implements AdapterView.OnItemClickListener, DatePickerDialog.OnDateChangedListener {
-	
-    private YearAdapter mAdapter;
-	private int mChildSize;
+public class YearPickerView extends LinearLayout implements AdapterView.OnItemClickListener, DatePickerDialog.OnDateChangedListener, AbsListView.OnScrollListener {
+
+	private ListView mListView;
+	private YearAdapter mAdapter;
+
+	private View mMoreContentIndicator;
+	private boolean mUseContentIndicator;
+
 	private final DatePickerController mController;
-	private TextViewWithCircularIndicator mSelectedView;
 	private int mViewSize;
 
 	public YearPickerView(Context context, DatePickerController datePickerController) {
@@ -30,18 +34,29 @@ public class YearPickerView extends ListView implements AdapterView.OnItemClickL
 		mController = datePickerController;
 		mController.registerOnDateChangedListener(this);
 
-		setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+		setOrientation(VERTICAL);
 
 		Resources resources = context.getResources();
 		mViewSize = resources.getDimensionPixelOffset(R.dimen.date_picker_view_animator_height);
-		mChildSize = resources.getDimensionPixelOffset(R.dimen.year_label_height);
 
-		setVerticalFadingEdgeEnabled(true);
-		setFadingEdgeLength(mChildSize / 3);
-		init(context);
-		setOnItemClickListener(this);
-		setSelector(new StateListDrawable());
-		setDividerHeight(0);
+		mListView = new ListView(context);
+
+		// Ensures that the selector on the text view is used.
+		mListView.setSelector(android.R.color.transparent);
+		mListView.setCacheColorHint(Color.TRANSPARENT);
+
+        LayoutParams listLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, 0);
+        listLayoutParams.weight = 1;
+		addView(mListView, listLayoutParams);
+
+		mUseContentIndicator = true;
+		mMoreContentIndicator = LayoutInflater.from(context).inflate(R.layout.year_picker_footer, this, false);
+		addView(mMoreContentIndicator);
+
+		init();
+		mListView.setOnScrollListener(this);
+		mListView.setOnItemClickListener(this);
+		mListView.setDividerHeight(0);
 		onDateChanged();
 	}
 
@@ -49,13 +64,24 @@ public class YearPickerView extends ListView implements AdapterView.OnItemClickL
         return Integer.valueOf(view.getText().toString());
     }
 
-	private void init(Context context) {
-		ArrayList<String> years = new ArrayList<String>();
+	private void init() {
+		ArrayList<Integer> years = new ArrayList<Integer>();
 		for (int year = mController.getMinYear(); year <= mController.getMaxYear(); year++) {
-			years.add(String.format("%d", year));
+			years.add(year);
 		}
-		mAdapter = new YearAdapter(context, R.layout.year_label_text_view, years);
-		setAdapter(mAdapter);
+		mAdapter = new YearAdapter(years);
+		mListView.setAdapter(mAdapter);
+
+		// Check whether the 'more content indicator should be visible at all'
+		if (isAtBottom()) {
+			mUseContentIndicator = false;
+			mMoreContentIndicator.setVisibility(GONE);
+		}
+	}
+
+	private boolean isAtBottom() {
+		return mListView.getLastVisiblePosition() == mListView.getAdapter().getCount() -1 &&
+				mListView.getChildAt(mListView.getChildCount() - 1).getBottom() <= mListView.getHeight();
 	}
 
 	public int getFirstPositionOffset() {
@@ -71,73 +97,108 @@ public class YearPickerView extends ListView implements AdapterView.OnItemClickL
 		postSetSelectionCentered(mController.getSelectedDay().year - mController.getMinYear());
 	}
 
-
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         mController.tryVibrate();
-        TextViewWithCircularIndicator clickedView = (TextViewWithCircularIndicator) view;
+        TextView clickedView = (TextView) view;
         if (clickedView != null) {
-            if (clickedView != mSelectedView) {
-                if (mSelectedView != null) {
-                    mSelectedView.drawIndicator(false);
-                    mSelectedView.requestLayout();
-                }
-                clickedView.drawIndicator(true);
-                clickedView.requestLayout();
-                mSelectedView = clickedView;
-            }
             mController.onYearSelected(getYearFromTextView(clickedView));
             mAdapter.notifyDataSetChanged();
         }
 	}
 
 	public void postSetSelectionCentered(int position) {
-		postSetSelectionFromTop(position, mViewSize / 2 - mChildSize / 2);
+		postSetSelectionFromTop(position, mViewSize / 3);
 	}
 
 	public void postSetSelectionFromTop(final int position, final int y) {
 		post(new Runnable() {
 			public void run() {
-				setSelectionFromTop(position, y);
+				mListView.setSelectionFromTop(position, y);
 				requestLayout();
 			}
 		});
 	}
 
-	private class YearAdapter extends ArrayAdapter<String> {
-		ColorStateList textColorStateList;
-
-        public YearAdapter(Context context, int resource, List<String> years) {
-            super(context, resource, years);
-			textColorStateList = createTextColorStateList(context);
-		}
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            TextViewWithCircularIndicator v = (TextViewWithCircularIndicator) super.getView(position, convertView, parent);
-			v.setTextColor(textColorStateList);
-            v.requestLayout();
-
-            int year = getYearFromTextView(v);
-            boolean selected = mController.getSelectedDay().year == year;
-            v.drawIndicator(selected);
-            if (selected) {
-                mSelectedView = v;
-            }
-            return v;
+    public int getFirstVisiblePosition() {
+        if (mListView == null) {
+            return 0;
         }
+        return mListView.getFirstVisiblePosition();
+    }
 
-		private ColorStateList createTextColorStateList(Context context) {
-			int[][] states = new int[][]{
-					new int[]{-android.R.attr.state_selected, -android.R.attr.state_pressed}, // not selected, and not pressed
-					new int[]{android.R.attr.state_pressed}, // pressed
-			};
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	}
 
-			int[] colors = new int[]{
-					context.getResources().getColor(R.color.date_picker_text_normal),
-					Utils.getPrimaryDarkColor(context)
-			};
-
-			return new ColorStateList(states, colors);
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		if (!mUseContentIndicator) {
+			// Don't do anything since the indicator is never visible.
+			return;
 		}
+
+		final int lastItem = firstVisibleItem + visibleItemCount;
+		if (lastItem == totalItemCount && isAtBottom()) {
+			mMoreContentIndicator.setVisibility(GONE);
+		} else {
+			mMoreContentIndicator.setVisibility(VISIBLE);
+		}
+	}
+
+	private class YearAdapter extends BaseAdapter {
+		private static final int DEFAULT_ITEM_TYPE = 0;
+		private static final int SELECTED_ITEM_TYPE = 1;
+		private List<Integer> years;
+
+		public YearAdapter(List<Integer> years) {
+			this.years = years;
+		}
+
+		@Override
+		public int getCount() {
+			return years.size();
+		}
+
+		@Override
+		public Integer getItem(int position) {
+			return years.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			if (getItem(position) == mController.getSelectedDay().year) {
+				return SELECTED_ITEM_TYPE;
+			}
+			return DEFAULT_ITEM_TYPE;
+		}
+
+		@Override
+		public int getViewTypeCount() {
+			return 2;
+		}
+
+		@Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				int layoutId;
+				if (getItemViewType(position) == SELECTED_ITEM_TYPE) {
+					layoutId = R.layout.year_label_text_view_selected;
+				} else {
+					layoutId = R.layout.year_label_text_view;
+				}
+				convertView = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+			}
+			Integer year = getItem(position);
+
+			TextView yearLabel = (TextView) convertView;
+			yearLabel.setText(String.format("%d", year));
+
+            return convertView;
+        }
 	}
 }
